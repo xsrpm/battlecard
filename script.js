35ba901ecc21adb9ff064d84e80673537b909cc6
César Palma Morante <cemp2703@gmail.com>
2020-10-18 18:22:22 -0500

    Web Socket - Unirse a Sala de espera

--- script.js
+++ script.js
@@ -1,21 +1,58 @@
-function cambiarPantalla(nueva,actual){
-  actual.classList.toggle("oculto")
-  nueva.classList.toggle("oculto")
+const Pantalla = { EN_SALA_DE_ESPERA: "EN SALA DE ESPERA", JUEGO: "JUEGO", FIN_DE_JUEGO: "FIN DE JUEGO"}
+Object.freeze(Pantalla)
+const DialogoJuego = {
+    EN_SALA_DE_ESPERA: "EN SALA DE ESPERA",
+    JUEGO: "JUEGO",
+    FIN_DE_JUEGO: "FIN DE JUEGO",
+    OPCIONES_EN_TURNO:"OPCIONES EN TURNO",
+
+    SELECCIONAR_MANO:"SELECCIONAR_MANO",
+    SELECCIONAR_ZONABATALLA:"SELECCIONAR_ZONABATALLA",
+    SELECCIONAR_POSICIONBATALLA:"SELECCIONAR_POSICIONBATALLA",
+    SELECCIONAR_POSICIONBATALLAE:"SELECCIONAR_POSICIONBATALLAE",
+
+    CARTA_COLOCADA:"CARTA_COLOCADA",
+    ATAQUE_CARTA_REALIZADO:"ATAQUE_CARTA_REALIZADO",
+    ATAQUE_BARRERA_REALIZADO:"ATAQUE_BARRERA_REALIZADO",
+    CAMBIODEPOSICION_REALIZADO:"CAMBIODEPOSICION_REALIZADO",
+    //FINALES
+    JUGADORSINCARTASBARRERA:"JUGADORSINCARTASBARRERA",
+    JUGADORSINCARTASMAZO:"JUGADORSINCARTASMAZO",
 }
+
+const pantallas = document.querySelectorAll("body > div")
 const bienvenida=document.querySelector(".bienvenida")
 const recepcion=document.querySelector(".recepcion")
 const sala=document.querySelector(".sala")
+const h2 = sala.getElementsByTagName("h2")
 
+const nombreJugador=document.getElementById("nombreJugador")
+const btnUnirASala=document.getElementById("btnUnirASala")
 const btnJugar = document.getElementById("btnJugar")
+
+let uuid
+
+function cambiarPantalla(pantalla){
+  Array.from(pantallas).forEach(p=>{
+    p.style.zIndex=0
+  })
+  pantalla.style.zIndex=1
+}
+
 btnJugar.addEventListener("click",()=>{
-  cambiarPantalla(recepcion,bienvenida)
+  cambiarPantalla(recepcion)
+})
+btnUnirASala.addEventListener("click",()=>{
+  socket.send(JSON.stringify({"accion":"Unir A Sala","nombreJugador":nombreJugador.value}));
 })
 
+let url = location.host == 'localhost' ?
+  'ws://localhost:8080/ws' : 'ws://battlecard-api.cemp2703.repl.co/ws'
 
-const socket = new WebSocket("wss://battlecard-api.cemp2703.repl.co/ws");
+
+const socket = new WebSocket(url);
 socket.onopen = e=>{
-  console.log("Conexión abierta")
-  socket.send("Conexión abierta");
+  socket.send(JSON.stringify({mensaje:"Abriendo socket"}));
 };
 socket.onerror= e=>{
   if(!recepcion.disabled){
@@ -25,47 +62,23 @@ socket.onerror= e=>{
   console.log("Error: "+e)
 }
 socket.onmessage = e=> {
-  if(!recepcion.disabled){
-    console.log(e.data);
-    cambiarPantalla(sala,recepcion)
+  let objData = JSON.parse(e.data)
+  console.log(objData)
+  if(objData.pantalla === Pantalla.EN_SALA_DE_ESPERA){
+    for(let i=0;i<objData.jugadorNombre.length;i++){
+      h2[i].innerText= objData.jugadorNombre[i]
+    }
+    if(typeof objData.uuid !== 'undefined'){
+      uuid = objData.uuid
+      console.log(uuid)
+      cambiarPantalla(sala)  
+    }
   }
 }
-socket.onclose=e=>{}
-
-function status(response) {
-  if (response.status >= 200 && response.status < 300) {
-    return Promise.resolve(response)
-  } else {
-    return Promise.reject(new Error(response.statusText))
-  }
+socket.onclose=e=>{
+  console.log("Cerrando socket "+e);
 }
 
-const btnUnirASala=document.getElementById("btnUnirASala")
-const nombreJugador=document.getElementById("nombreJugador")
-btnUnirASala.addEventListener("click",()=>{
-  //btnUnirASala.innerText="Cargango..."
-  //btnUnirASala.setAttribute("disabled","true")
-   /*
-  fetch('https://battlecard-api.cemp2703.repl.co/api/sala/unirse', {
-    mode: 'cors',
-    method: 'post',
-    body:{'nombreJugador':nombreJugador}
-  }).then(status)
-  .then(response=> response.json())
-  .then(data=>{
-    console.log(data)
-    btnUnirASala.innerText="Unirse a la Sala"
-    btnUnirASala.setAttribute("disabled","false")
-    cambiarPantalla(sala,recepcion)
-  })
-  .catch((error)=> {
-    console.log('Request failed', error);
-    alert('Request failed: '+error)
-    btnUnirASala.innerText="Unirse a la Sala"
-    btnUnirASala.setAttribute("disabled","false")
-  })
-  */
- socket.send(JSON.stringify({'nombreJugador':nombreJugador}));
- 
-})
 
+
+/*Se podría abrir la conexión con el socket server al hacer click en el boton unir a la sala*/
