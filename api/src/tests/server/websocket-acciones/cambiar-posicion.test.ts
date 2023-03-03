dc4c77ce9d5ea6cdf94192da70054ded642cda6c
César Palma Morante <cemp2703@gmail.com>
2023-03-02 23:59:50 -0500

    backend: ws test cambiar posicion

---
+++ api/src/tests/server/websocket-acciones/cambiar-posicion.test.ts
@@ -0,0 +1,68 @@
+import request from "superwstest";
+import {
+  iniciarJuego,
+  unirseASala1,
+  unirseASala2,
+  terminarTurno,
+  colocarCarta,
+  cambiarPosicion,
+} from "../../../utils/websocket-test-helper";
+import { UnirASalaResponse } from "../../../response";
+import server from "../../../server/websocket-acciones";
+import { PosBatalla } from "../../../constants/celdabatalla";
+import { WebsocketEventTitle } from "../../../constants/websocket-event-title";
+
+describe("Websocket Server", () => {
+  beforeEach((done) => {
+    server.listen(0, "localhost", done);
+  });
+
+  afterEach((done) => {
+    server.close(done);
+  });
+
+  describe("estando el servidor en sala de espera", () => {
+    describe("cambiar posición", () => {
+      test("válido", async () => {
+        let jugadorId1 = "",
+          jugadorId2 = "",
+          idZonaBatalla = 0,
+          idMano = 0;
+        await request(server)
+          .ws("/ws")
+          .sendJson(unirseASala1)
+          .expectJson((response: UnirASalaResponse) => {
+            jugadorId1 = response.payload.jugadorId as string;
+          })
+          .exec(() => {
+            request(server)
+              .ws("/ws")
+              .sendJson(unirseASala2)
+              .expectJson((response: UnirASalaResponse) => {
+                jugadorId2 = response.payload.jugadorId as string;
+              })
+              .sendJson(iniciarJuego(jugadorId2));
+          })
+          .expectJson()
+          .expectJson()
+          .sendJson(
+            colocarCarta(jugadorId1, PosBatalla.ATAQUE, idZonaBatalla, idMano)
+          )
+          .expectJson()
+          .sendJson(terminarTurno(jugadorId1))
+          .expectJson()
+          .exec(() => {
+            request(server).ws("/ws").sendJson(terminarTurno(jugadorId2));
+          })
+          .sendJson(cambiarPosicion(jugadorId1, idZonaBatalla))
+          .expectJson({
+            event: WebsocketEventTitle.CAMBIAR_POSICION,
+            payload: {
+              jugadorId: jugadorId1,
+              idZonaBatalla
+            },
+          });
+      });
+    });
+  });
+});
--- api/src/utils/websocket-test-helper.ts
+++ api/src/utils/websocket-test-helper.ts
@@ -1,6 +1,10 @@
-import { ResultadoAtacarCarta, ResultadoAtacarBarrera, ResultadoCambiarPosicion } from './../constants/jugador';
-import { ResultadoColocarCarta } from '../constants/jugador';
-import { WebsocketEventTitle } from '../constants/websocket-event-title';
+import {
+  ResultadoAtacarCarta,
+  ResultadoAtacarBarrera,
+  ResultadoCambiarPosicion,
+} from "./../constants/jugador";
+import { ResultadoColocarCarta } from "../constants/jugador";
+import { WebsocketEventTitle } from "../constants/websocket-event-title";
 
 export const nombreJugador1 = "César";
 export const unirseASala1 = {
@@ -17,60 +21,79 @@ export const unirseASala2 = {
     nombreJugador: nombreJugador2,
   },
 };
-export const iniciarJuego = (jugadorId:string) =>({
-    event: WebsocketEventTitle.INICIAR_JUEGO,
-    payload: {
-      jugadorId
-    }
-  })
+export const iniciarJuego = (jugadorId: string) => ({
+  event: WebsocketEventTitle.INICIAR_JUEGO,
+  payload: {
+    jugadorId,
+  },
+});
 
-  export const seleccionarMano = (jugadorId:string, idMano: number) => ({
-    event: WebsocketEventTitle.SELECCIONAR_MANO,
-    payload: {
-      jugadorId,
-      idMano
-    }
-  })
+export const seleccionarMano = (jugadorId: string, idMano: number) => ({
+  event: WebsocketEventTitle.SELECCIONAR_MANO,
+  payload: {
+    jugadorId,
+    idMano,
+  },
+});
 
-  export const seleccionarManoResponse = {
-    event: WebsocketEventTitle.SELECCIONAR_MANO,
-    payload: {
-      existeCarta: true,
-      puedeColocarCarta: ResultadoColocarCarta.POSIBLE
-    }
-  }
+export const seleccionarManoResponse = {
+  event: WebsocketEventTitle.SELECCIONAR_MANO,
+  payload: {
+    existeCarta: true,
+    puedeColocarCarta: ResultadoColocarCarta.POSIBLE,
+  },
+};
 
-  export const seleccionarCeldaEnZonaBatalla = (jugadorId:string, idZonaBatalla: number)=>({
-      event: WebsocketEventTitle.SELECCIONAR_ZONA_BATALLA,
-      payload: {
-        jugadorId,
-        idZonaBatalla
-      }
-  })
+export const seleccionarCeldaEnZonaBatalla = (
+  jugadorId: string,
+  idZonaBatalla: number
+) => ({
+  event: WebsocketEventTitle.SELECCIONAR_ZONA_BATALLA,
+  payload: {
+    jugadorId,
+    idZonaBatalla,
+  },
+});
 
-  export const seleccionarCeldaEnZonaBatallaResponse = {
-    event: WebsocketEventTitle.SELECCIONAR_ZONA_BATALLA,
-    payload: {
-        existeCarta: true,
-        puedeAtacarCarta: ResultadoAtacarCarta.ATAQUES_SOLO_SE_REALIZAN_EN_SEGUNDO_TURNO,
-        puedeAtacarBarrera: ResultadoAtacarBarrera.ATAQUES_SOLO_SE_REALIZAN_EN_SEGUNDO_TURNO,
-        puedeCambiarPosicion: ResultadoCambiarPosicion.SIN_CAMBIOS_DE_POSICION_DISPONIBLES
-    }
-}
+export const seleccionarCeldaEnZonaBatallaResponse = {
+  event: WebsocketEventTitle.SELECCIONAR_ZONA_BATALLA,
+  payload: {
+    existeCarta: true,
+    puedeAtacarCarta:
+      ResultadoAtacarCarta.ATAQUES_SOLO_SE_REALIZAN_EN_SEGUNDO_TURNO,
+    puedeAtacarBarrera:
+      ResultadoAtacarBarrera.ATAQUES_SOLO_SE_REALIZAN_EN_SEGUNDO_TURNO,
+    puedeCambiarPosicion:
+      ResultadoCambiarPosicion.SIN_CAMBIOS_DE_POSICION_DISPONIBLES,
+  },
+};
 
-export const colocarCarta = (jugadorId:string, posicion: string, idZonaBatalla: number, idMano: number)=>({
-    event: WebsocketEventTitle.COLOCAR_CARTA,
-    payload: {
-      jugadorId,
-      posicion,
-      idZonaBatalla,
-      idMano
-    }
-  })
+export const colocarCarta = (
+  jugadorId: string,
+  posicion: string,
+  idZonaBatalla: number,
+  idMano: number
+) => ({
+  event: WebsocketEventTitle.COLOCAR_CARTA,
+  payload: {
+    jugadorId,
+    posicion,
+    idZonaBatalla,
+    idMano,
+  },
+});
 
-  export const terminarTurno = (jugadorId: string)=>({
-    event: WebsocketEventTitle.TERMINAR_TURNO,
-    payload: {
-      jugadorId
-    }
-  })
+export const terminarTurno = (jugadorId: string) => ({
+  event: WebsocketEventTitle.TERMINAR_TURNO,
+  payload: {
+    jugadorId,
+  },
+});
+
+export const cambiarPosicion = (jugadorId: string, idZonaBatalla: number) => ({
+  event: WebsocketEventTitle.CAMBIAR_POSICION,
+  payload: {
+    jugadorId,
+    idZonaBatalla,
+  },
+});
